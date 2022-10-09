05770f09825ae5627e24f7797ffd855f01ab5a34
Wiktor <victortkd88@gmail.com>
2022-10-08 20:56:59 -0600

    Resposive design 70%

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const btn = document.getElementById('btn');
 const menu = document.querySelector('.menu-ol_bar');
 
 btn.addEventListener('click', (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   menu.classList.toggle('menu-bar_active')
 });
